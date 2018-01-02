7608b70c980aca0ebc5d61e2316b1bad0ac49131
Dietmar Maurer <dietmar@proxmox.com>
2018-01-02 10:34:29 +0100

    gen-pmg.conf.5-opts.pl: allow to generate single section config

--- gen-pmg.conf.5-opts.pl
+++ gen-pmg.conf.5-opts.pl
@@ -11,12 +11,21 @@ use PMG::Config;
 
 my $types = PMG::Config::Base->lookup_types;
 
+my $single_section = shift;
+my $found = 0;
+
 foreach my $section (@$types) {
     my $plugin = PMG::Config::Base->lookup($section);
     my $schema = $plugin->updateSchema(1);
     my $properties = $schema->{properties};
-    
-    print ".Section '$section'\n\n";
+
+    if (defined($single_section)) {
+	next if $section ne $single_section;
+    } else {
+	print ".Section '$section'\n\n";
+    }
+
+    $found = 1;
 
     my $filter = sub {
 	my ($key, $phash) = @_;
@@ -27,6 +36,10 @@ foreach my $section (@$types) {
     
     print PVE::RESTHandler::dump_properties(
 	$properties, 'asciidoc', 'config', $filter);
+}
+
+if (defined($single_section)) {
+    die "no such section '$single_section'" if !$found;
 }
  
 exit(0);
