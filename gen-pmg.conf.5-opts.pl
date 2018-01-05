e09057abe28e7167a909195952d8b201f334c4ba
Dietmar Maurer <dietmar@proxmox.com>
2018-01-05 09:20:14 +0100

    gen-pmg.conf.5-opts.pl: allow to generate docs for subgroups

--- gen-pmg.conf.5-opts.pl
+++ gen-pmg.conf.5-opts.pl
@@ -14,6 +14,27 @@ my $types = PMG::Config::Base->lookup_types;
 my $single_section = shift;
 my $found = 0;
 
+my $key_groups = {
+    'mail-relaying' => [
+	'mail' , {
+	    relay => 1,
+	    relaynomx => 1,
+	    relayport => 1,
+	    smarthost => 1,
+	}],
+};
+
+my $select_keys;
+
+if ($single_section) {
+    if (my $a =  $key_groups->{$single_section}) {
+	my ($sec, $hash) = @$a;
+	$single_section = $sec;
+	$select_keys = $hash;
+    }
+}
+
+
 foreach my $section (@$types) {
     my $plugin = PMG::Config::Base->lookup($section);
     my $schema = $plugin->updateSchema(1);
@@ -31,6 +52,7 @@ foreach my $section (@$types) {
 	my ($key, $phash) = @_;
 	return 1 if $key eq 'digest';
 	return 1 if $key eq 'delete';
+	return 1 if $select_keys && !$select_keys->{$key};
 	return 0;
     };
     
